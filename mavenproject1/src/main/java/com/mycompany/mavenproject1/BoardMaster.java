927553143fa642351635ede675476654b97b4bb8
Evandleclair <evandleclair@gmail.com>
2022-05-05 12:56:01 -0400

    Basic outline of program created: -Boardmaster will hold and manage the board. -CellAutomaton is an abstract class that Board bases itself off of. -Simulator is the main class.

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/BoardMaster.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/BoardMaster.java
@@ -5,13 +5,59 @@
 
 package com.mycompany.mavenproject1;
 
+import java.util.Arrays;
+
 /**
  *
  * @author toast
  */
-public class BoardMaster {
+public class BoardMaster extends CellAutomaton{
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    int boardstate[][];
+    int dimensions;
+    double probAlive=0.5;
+    
+    public BoardMaster(int d)
+    {
+        //boardstate = new int[d][d];
+        
+        dimensions=d;
     }
-}
+    public void setupBoard()
+    {
+        deadState(dimensions,dimensions);
+        seedBoard();
+        reportBoard();
+    }
+    public void deadState(int width, int height)
+    {
+        boardstate= new int[width][height];
+    }
+    public void reportBoard()
+    {
+        if (boardstate!=null)
+        {
+            System.out.println(Arrays.deepToString(boardstate));
+        }
+    }
+    public void seedBoard()
+    {
+        double randstate;
+        for(int r=0;r<dimensions;r++)
+        {
+        for(int c=0;c<dimensions;c++)
+            {
+             randstate = Math.random();
+            if (randstate>probAlive)
+                {
+                boardstate[r][c]=1;
+                }
+            }
+        }
+    }//end seedboard//
+    @Override
+     public boolean rules(int num){
+        return num == 3 || num == 12 || num == 13;
+    }
+
+}//end boardmaster//
---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Simulator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author toast
+ */
+public class Simulator {
+    public static void main(String args[]) throws InterruptedException
+    {
+        var ourBoard = new BoardMaster(50);
+        ourBoard.setupBoard();
+    }
+}
